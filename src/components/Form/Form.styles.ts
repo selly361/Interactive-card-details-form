8d7b948f784f1959db9c79c67ffc6460a4f27c50
selly361 <104414846+selly361@users.noreply.github.com>
2023-02-20 13:20:41 +0000

    Fixed: the height of the sidebar not fitting the screen on larger devices

--- src/components/Form/Form.styles.ts
+++ src/components/Form/Form.styles.ts
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const StyledForm = styled.form`
+  position: absolute;
   height: 352px;
   width: 381px;
   left: 832px;
   top: 275px;
-  border-radius: 0px;
   border: 1px solid black;
 `;
