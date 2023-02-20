8f6881e38de1ff62f5ef73c47d8d1420819d6ab0
selly361 <104414846+selly361@users.noreply.github.com>
2023-02-20 14:03:01 +0000

    Change: finished designing Form

--- src/components/Form/Form.styles.ts
+++ src/components/Form/Form.styles.ts
@@ -1,10 +1,96 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const StyledForm = styled.form`
+  position: absolute;
   height: 352px;
   width: 381px;
   left: 832px;
   top: 275px;
-  border-radius: 0px;
-  border: 1px solid black;
+  display: flex;
+  flex-flow: column;
+  gap: 26px;
+`;
+
+export const StyledLabel = styled.label`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 15px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  color: #21092f;
+`;
+
+const commonInputStyles = css`
+  background: #ffffff;
+  border: 1px solid #dfdee0;
+  border-radius: 8px;
+  padding: 11px 0px 11px 16px;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 23px;
+  color: #21092f;
+  mix-blend-mode: normal;
+  transition: 1s ease border-color;
+
+  &:focus {
+    border: 1px solid #6348fe;
+  }
+
+  &.error {
+    border: 1px solid #FF5050;
+  }
+`;
+
+export const LargeInput = styled.input`
+  ${commonInputStyles}
+  height: 45px;
+  width: 381px;
+`;
+
+export const MediumInput = styled.input`
+  ${commonInputStyles}
+  height: 45px;
+  width: 191px;
+`;
+
+export const SmallInput = styled.input`
+  ${commonInputStyles}
+  height: 45px;
+  width: 80px;
+`;
+
+export const Wrapper = styled.fieldset`
+  border: unset;
+  display: grid;
+  gap: 9px;
+  justify-items: start;
+`;
+
+export const Container = styled.fieldset`
+  border: unset;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  justify-items: start;
+  width: 170px;
+`;
+
+export const StyledButton = styled.button`
+  background: #21092f;
+  border-radius: 8px;
+  height: 53px;
+  width: 381px;
+  display: grid;
+  place-items: center;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 23px;
+  color: #ffffff;
+  cursor: pointer;
+  transition: 1s ease background;
+
+
+  &:hover {
+    background: #6348fe;
+  }
 `;
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import { StyledForm } from './Form.styles'
+import { Container, LargeInput, MediumInput, SmallInput, StyledButton, StyledForm, StyledLabel, Wrapper } from "./Form.styles";
+
+import React from "react";
 
 function Form() {
   return (
-    <StyledForm></StyledForm>
-  )
+    <StyledForm>
+      <Wrapper>
+        <StyledLabel>Cardholder Name</StyledLabel>
+        <LargeInput placeholder="e.g. Jane Appleseed" />
+      </Wrapper>
+      <Wrapper>
+        <StyledLabel>Card Number</StyledLabel>
+        <LargeInput placeholder="e.g. 1234 5678 9123 0000" />
+      </Wrapper>
+      <Container>
+        <Wrapper>
+          <StyledLabel>Exp. Date (MM/YY)</StyledLabel>
+          <Container>
+            <SmallInput placeholder="MM" />
+            <SmallInput placeholder="YY" />
+          </Container>
+        </Wrapper>
+        <Wrapper>
+          <StyledLabel>CVC</StyledLabel>
+          <MediumInput placeholder="e.g. 123" />
+        </Wrapper>
+      </Container>
+      <StyledButton>Confirm</StyledButton>
+    </StyledForm>
+  );
 }
 
-export default Form
+export default Form;
