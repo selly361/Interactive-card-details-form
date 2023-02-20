71b3e7bb0a7c76047789e7fa6077f156e335b3fb
selly361 <104414846+selly361@users.noreply.github.com>
2023-02-20 13:21:29 +0000

    Fixed: the height of the sidebar not being the height of the screen

--- src/components/SideBar/SideBar.styles.ts
+++ src/components/SideBar/SideBar.styles.ts
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const StyledAside = styled.aside`
   width: 483px;
-  height: 900px;
-  background: url(images/bg-main-desktop.png);
+  min-height: 100vh;
+  background: url(images/bg-main-desktop.png) no-repeat;
+  background-size: cover;
   position: absolute;
   left: 0;
   top: 0;
