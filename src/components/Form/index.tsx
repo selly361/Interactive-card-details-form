70afb8c0832a4e884e69ee47e8e71a00ccc7311b
seikou <seikou615@gmail.com>
2023-02-28 12:40:23 +0000

    Change: import

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,42 +1,33 @@
-import {
-  Container,
-  LargeInput,
-  MediumInput,
-  SmallInput,
-  StyledButton,
-  StyledForm,
-  StyledLabel,
-  Wrapper,
-} from "./Form.styles";
+import * as styles from "./Form.styles";
 
 import React from "react";
 
 function Form() {
   return (
-    <StyledForm>
-      <Wrapper>
-        <StyledLabel htmlFor="cardholder-name">Cardholder Name</StyledLabel>
-        <LargeInput id="cardholder-name" placeholder="e.g. Jane Appleseed" />
-      </Wrapper>
-      <Wrapper>
-        <StyledLabel htmlFor="card-number">Card Number</StyledLabel>
-        <LargeInput id="card-number" placeholder="e.g. 1234 5678 9123 0000" />
-      </Wrapper>
-      <Container>
-        <Wrapper>
-          <StyledLabel htmlFor="expiry-date">Exp. Date (MM/YY)</StyledLabel>
-          <Container>
-            <SmallInput id="expiry-date" placeholder="MM" />
-            <SmallInput placeholder="YY" />
-          </Container>
-        </Wrapper>
-        <Wrapper>
-          <StyledLabel htmlFor="cvc">CVC</StyledLabel>
-          <MediumInput id="cvc" placeholder="e.g. 123" />
-        </Wrapper>
-      </Container>
-      <StyledButton  type="submit">Confirm</StyledButton>
-    </StyledForm>
+    <styles.StyledForm>
+      <styles.Wrapper>
+        <styles.StyledLabel htmlFor="cardholder-name">Cardholder Name</styles.StyledLabel>
+        <styles.LargeInput id="cardholder-name" placeholder="e.g. Jane Appleseed" />
+      </styles.Wrapper>
+      <styles.Wrapper>
+        <styles.StyledLabel htmlFor="card-number">Card Number</styles.StyledLabel>
+        <styles.LargeInput id="card-number" placeholder="e.g. 1234 5678 9123 0000" />
+      </styles.Wrapper>
+      <styles.Container>
+        <styles.Wrapper>
+          <styles.StyledLabel htmlFor="expiry-date">Exp. Date (MM/YY)</styles.StyledLabel>
+          <styles.Container>
+            <styles.SmallInput id="expiry-date" placeholder="MM" />
+            <styles.SmallInput placeholder="YY" />
+          </styles.Container>
+        </styles.Wrapper>
+        <styles.Wrapper>
+          <styles.StyledLabel htmlFor="cvc">CVC</styles.StyledLabel>
+          <styles.MediumInput id="cvc" placeholder="e.g. 123" />
+        </styles.Wrapper>
+      </styles.Container>
+      <styles.StyledButton  type="submit">Confirm</styles.StyledButton>
+    </styles.StyledForm>
   );
 }
 
