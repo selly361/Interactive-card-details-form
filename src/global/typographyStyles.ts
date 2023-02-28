d5921c38bb606998dcb3ae65c43e3d1bb1b2e8d1
seikou <seikou615@gmail.com>
2023-02-28 13:30:14 +0000

    Added: typography styles (eg. HeadingXL, HeadingL.. )

---
+++ src/global/typographyStyles.ts
@@ -0,0 +1,29 @@
+import { css } from "styled-components";
+
+export const HeadingXL = css`
+  font-size: 28px;
+  font-weight: 500;
+  line-height: 36px;
+  letter-spacing: 3.422222137451172px;
+`;
+
+export const HeadingL = css`
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 23px;
+  letter-spacing: 0px;
+`;
+
+export const BodyM = css`
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 15px;
+  letter-spacing: 2px;
+`;
+
+export const BodyL = css`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 18px;
+  letter-spacing: 2px;
+`;
